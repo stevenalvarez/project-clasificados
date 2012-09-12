33940f159852605e4d268a773bbb8c67431b95d5
hunter <danfercf@gmail.com>
2012-09-12 09:07:20 -0400

    Maqueta

---
+++ support/EncuentraBolivia/includes/header.php
@@ -0,0 +1,36 @@
+<div id="header">
+    <div class="main_wrap">
+        <div class="header_main">
+            <div class="logo">
+                <a href="">
+                    <img src="images/header_logo.png"/>
+                </a>
+            </div>
+            <div class="links">
+                <a href="">Iniciar Sesion</a>
+                <span>-</span>
+                <a href="">Registrarse</a>
+            </div>
+        </div>
+    </div>
+    <div class="main_banner">
+        <a href="">
+            <img src="images/banner1.png"/>
+        </a>
+        <form>
+            <div class="search_input">
+                <input type="text" name="search_string" value="Busqueda..."/>
+                <button type="submit"></button>
+            </div>
+            <div class="search_select">
+            <label>En:</label>
+                <select>
+                    <option>Inmuebles</option>
+                    <option>Compra / Venta en general</option>
+                    <option>Salud</option>
+                </select>
+            </div>
+        </form>
+    </div>
+</div>
+<div class="body_wrap">
