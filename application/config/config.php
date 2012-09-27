8c3939e40e42b92804992a7ac78ea48ff76fd486
Jhonny Alvarez <steven.alvarez.v@gmail.com>
2012-09-27 19:24:06 -0400

    test gitignire

--- application/config/config.php
+++ application/config/config.php
@@ -15,7 +15,7 @@
 |
 */
 $config['base_url']	= 'http://localhost/clasificados/';
-
+#$config['base_url']	= 'http://localhost/clasificados/';
 /*
 |--------------------------------------------------------------------------
 | Index File
